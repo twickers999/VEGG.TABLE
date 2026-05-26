0ec9fc156940f98a0adbb3042c43b14bacbe2cb6
leo <62425425+lleaob@users.noreply.github.com>
2026-05-26 16:24:04 +0100

    feat: Implement base classes.

---
+++ VEGG.TABLE.API/VEGG.TABLE.API/DataModels/Table.cs
@@ -0,0 +1,9 @@
+﻿namespace VEGG.TABLE.API.DataModels
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProduceId { get; set; }
+    }
+}
---
+++ VEGG.TABLE.API/VEGG.TABLE.API/DataModels/User.cs
@@ -0,0 +1,18 @@
+﻿namespace VEGG.TABLE.API.DataModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Produce> ListOfProduce { get; set; } = new List<Produce>();
+        public UserType UserType { get; set; } = UserType.Buyer;
+    }
+    public enum UserType
+    {
+        Seller,
+        Buyer,
+        Admin
+    }
+}
